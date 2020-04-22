49642813f6a26a65fbd46c13693e5dd830dd252c
MarionGl <marion.gloriant@gmail.com>
2020-04-22 11:10:40 +0200

    implémentation update et findAll StockServiceImpl

--- microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/service/StockServiceImpl.java
+++ microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/service/StockServiceImpl.java
@@ -2,40 +2,47 @@ package com.inti.formation.shop.api.service;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.inti.formation.shop.api.repository.StockRepository;
 import com.inti.formation.shop.api.repository.model.Stock;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+/**
+ * @author Antoine Bertin, Marion Gloriant
+ */
+
 @Component
 @Slf4j
 public class StockServiceImpl implements StockService{
 
+	@Autowired
+	private StockRepository stockRepository;
+	
 	@Override
-	public Mono<Stock> add(Stock s) {
+	public Mono<Stock> add(final Stock s) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Flux<Stock> searchDate(Date d) {
+	public Flux<Stock> searchDate(final Date d) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Mono<Stock> update(Stock s) {
-		// TODO Auto-generated method stub
-		return null;
+	public Mono<Stock> update(final Stock s) {
+		return stockRepository.save(s);
 	}
 
 	@Override
 	public Flux<Stock> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return stockRepository.findAll();
 	}
 
 }
