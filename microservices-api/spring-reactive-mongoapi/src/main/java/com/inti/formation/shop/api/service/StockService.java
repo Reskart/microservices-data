5826c763fe55a00f8a664f3f4f63e557c0ef815b
Antoine <antoinebertinb@gmail.com>
2020-04-22 10:04:42 +0200

    remplissage des classes services

--- microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/service/StockService.java
+++ microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/service/StockService.java
@@ -1,5 +1,20 @@
 package com.inti.formation.shop.api.service;
 
+import java.util.Date;
+
+import com.inti.formation.shop.api.repository.model.Stock;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface StockService {
+	
+	public Mono<Stock> add(Stock s);
+	
+	public Flux<Stock> searchDate(Date d);
+	
+	public Mono<Stock> update(Stock s);
+	
+	public Flux<Stock> findAll();
 
 }
--- microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/service/StockServiceImpl.java
+++ microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/service/StockServiceImpl.java
@@ -1,5 +1,36 @@
 package com.inti.formation.shop.api.service;
 
+import java.util.Date;
+
+import com.inti.formation.shop.api.repository.model.Stock;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public class StockServiceImpl implements StockService{
+
+	@Override
+	public Mono<Stock> add(Stock s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Flux<Stock> searchDate(Date d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Stock> update(Stock s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Flux<Stock> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
