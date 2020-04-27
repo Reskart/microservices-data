43f8e21def49acf3ca6dd20443bab8873da8c7db
MarionGl <marion.gloriant@gmail.com>
2020-04-27 13:44:31 +0200

    création classe KafkaConfig

--- microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/ApplicationShop.java
+++ microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/ApplicationShop.java
@@ -1,102 +1,29 @@
 package com.inti.formation.shop.api;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.streams.StreamsConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 
-import com.inti.formation.data.kafka.producer.ApplicationProducer;
-import com.inti.formation.data.kafka.producer.serde.JsonPOJOSerializer;
-import com.inti.formation.shop.api.repository.model.Stock;
+import com.inti.formation.shop.api.kafka.KafkaConfig;
+
 
 @SpringBootConfiguration
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @EnableReactiveMongoRepositories // activation de spring mongo reactive
-public class ApplicationShop {
-		@Value("${kafka.bootstrap-server}")
-	    private String kafkaBrokerUrl;
-
-	    @Value("${kafka.acks}")
-	    private String acks;
-
-	    @Value("${kafka.retries}")
-	    private String retries;
-
-	    @Value("${kafka.buffer-memory}")
-	    private String bufferMemory;
-
-	    @Value("${kafka.batch-size}")
-	    private String batchSize;
-
-	    @Value("${kafka.client-id}")
-	    private String clientId;
-
-	    @Value("${kafka.compression-type}")
-	    private String compressionType;
-
-	    @Value("${kafka.user}")
-	    private String user;
-
-	    @Value("${kafka.password}")
-	    private String password;
-
-	    private static final String SECURITY_PROTOCOL = "security.protocol";
-	    private static final String SASL_MECHANISM = "sasl.mechanism";
-	    private static final String SASL_JAAS_CONFIG = "sasl.jaas.config";
-
+public class ApplicationShop extends KafkaConfig {
+		
 	
     public static void main(String[] args) {
         SpringApplication.run(ApplicationShop.class, args);
 
     }
-    @Bean
-    public Map<String, Object> producerConfigs() {
-	Map<String, Object> conf = new HashMap<>();
-	conf.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerUrl);
-	conf.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-	conf.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonPOJOSerializer.class.getName());
-	// If the request fails, the producer can automatically retry,
-	conf.put(ProducerConfig.RETRIES_CONFIG, retries);
-	conf.put(ProducerConfig.ACKS_CONFIG, acks);
-	conf.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
-	conf.put(ProducerConfig.BUFFER_MEMORY_CONFIG, bufferMemory);
-	conf.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
-	conf.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, compressionType); // lz4 compression mode unused cpu
-	// Authentification
-	//conf.put(SECURITY_PROTOCOL, "SASL_PLAINTEXT");
-	//conf.put(SASL_MECHANISM, "SCRAM-SHA-512");
-	//conf.put(SASL_JAAS_CONFIG, "org.apache.kafka.common.security.scram.ScramLoginModule required username=" + user
-	//	+ " password=" + password + ";");
-	conf.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
-	conf.put(StreamsConfig.EXACTLY_ONCE, true); // controls that the message is send one time when ack is all
-	return conf;
-    }
-
-    @Bean
-    public ProducerFactory<Long, Stock> producerFactory() {
-	return new DefaultKafkaProducerFactory<>(producerConfigs()); 
-    }
-
-    @Bean
-    public KafkaTemplate<Long, Stock> kafkaTemplate() {
-	return new KafkaTemplate<>(producerFactory());
-    }
+    
 
 
 }
---
+++ microservices-api/spring-reactive-mongoapi/src/main/java/com/inti/formation/shop/api/kafka/KafkaConfig.java
@@ -0,0 +1,84 @@
+package com.inti.formation.shop.api.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.streams.StreamsConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import com.inti.formation.data.kafka.producer.serde.JsonPOJOSerializer;
+import com.inti.formation.shop.api.repository.model.Stock;
+
+public class KafkaConfig {
+	
+	@Value("${kafka.bootstrap-server}")
+    private String kafkaBrokerUrl;
+
+    @Value("${kafka.acks}")
+    private String acks;
+
+    @Value("${kafka.retries}")
+    private String retries;
+
+    @Value("${kafka.buffer-memory}")
+    private String bufferMemory;
+
+    @Value("${kafka.batch-size}")
+    private String batchSize;
+
+    @Value("${kafka.client-id}")
+    private String clientId;
+
+    @Value("${kafka.compression-type}")
+    private String compressionType;
+
+    @Value("${kafka.user}")
+    private String user;
+
+    @Value("${kafka.password}")
+    private String password;
+
+    private static final String SECURITY_PROTOCOL = "security.protocol";
+    private static final String SASL_MECHANISM = "sasl.mechanism";
+    private static final String SASL_JAAS_CONFIG = "sasl.jaas.config";
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+	Map<String, Object> conf = new HashMap<>();
+	conf.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerUrl);
+	conf.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+	conf.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonPOJOSerializer.class.getName());
+	// If the request fails, the producer can automatically retry,
+	conf.put(ProducerConfig.RETRIES_CONFIG, retries);
+	conf.put(ProducerConfig.ACKS_CONFIG, acks);
+	conf.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+	conf.put(ProducerConfig.BUFFER_MEMORY_CONFIG, bufferMemory);
+	conf.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
+	conf.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, compressionType); // lz4 compression mode unused cpu
+	// Authentification
+	//conf.put(SECURITY_PROTOCOL, "SASL_PLAINTEXT");
+	//conf.put(SASL_MECHANISM, "SCRAM-SHA-512");
+	//conf.put(SASL_JAAS_CONFIG, "org.apache.kafka.common.security.scram.ScramLoginModule required username=" + user
+	//	+ " password=" + password + ";");
+	conf.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
+	conf.put(StreamsConfig.EXACTLY_ONCE, true); // controls that the message is send one time when ack is all
+	return conf;
+    }
+
+    @Bean
+    public ProducerFactory<Long, Stock> producerFactory() {
+	return new DefaultKafkaProducerFactory<>(producerConfigs()); 
+    }
+
+    @Bean
+    public KafkaTemplate<Long, Stock> kafkaTemplate() {
+	return new KafkaTemplate<>(producerFactory());
+    }
+
+}
